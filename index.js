51b8ae2a7d1d0c6b1cc0e0dc845608724abd8dec
r3d-slayer <tanwarshivam49@gmail.com>
2025-03-27 00:38:40 +0530

    ................
    .

--- index.js
+++ index.js
@@ -65,3 +65,16 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", handleProjectScroll);
     handleProjectScroll(); // Run on page load
 });
+
+
+function toggleMenu() {
+    const options = document.querySelector('.options');
+    const menuToggle = document.querySelector('.menu-toggle');
+
+    // Toggle the dropdown menu visibility
+    options.classList.toggle('show');
+    
+    // Toggle the "open" class to animate the hamburger to a cross
+    menuToggle.classList.toggle('open');
+}
+
